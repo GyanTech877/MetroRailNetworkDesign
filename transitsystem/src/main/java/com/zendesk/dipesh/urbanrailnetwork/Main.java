22307c1b5141b4c9d9aaa27705f8d170aec87c40
dipesh.bhakat <dipesh.bhakat@servicenow.com>
2019-08-23 00:31:26 +0530

    InitialCommit building project structure and adding sample data

---
+++ transitsystem/src/main/java/com/zendesk/dipesh/urbanrailnetwork/Main.java
@@ -0,0 +1,9 @@
+package com.zendesk.dipesh.urbanrailnetwork;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ transitsystem/src/main/java/com/zendesk/dipesh/urbanrailnetwork/exception/UrbalRailNetworkException.java
@@ -0,0 +1,9 @@
+package com.zendesk.dipesh.urbanrailnetwork.exception;
+
+public class UrbalRailNetworkException extends Exception{
+
+	private static final long serialVersionUID = 6446584189863313523L;
+	public UrbalRailNetworkException(String msg) {
+		super(msg);
+	}
+}
